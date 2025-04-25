ee1928257f6ecf0880d78e18d4d17c0a2f450d8b
wndltkr <zczb123@gmail.com>
2025-04-25 14:41:09 +0900

    상단 섹션 컴포넌트화 작업

--- src/app/guide/parking/page.tsx
+++ src/app/guide/parking/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { motion } from 'framer-motion';
-import Image from 'next/image';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import Menu from '@/components/Menu';
